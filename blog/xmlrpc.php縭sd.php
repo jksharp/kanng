2662414f883b7be130f304fcee27517cb8c1ad9e
jksharp <jiangkang_3@hotmail.com>
2015-05-08 17:56:54 +0800

    Signed-off-by: jksharp <jiangkang_3@hotmail.com>

---
+++ blog/xmlrpc.php縭sd.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+			<rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+				<service>
+					<engineName>emlog</engineName>
+					<engineLink>http://emlog.net/</engineLink>
+					<homePageLink>http://kanng.net/blog/</homePageLink>
+					<apis>
+						<api name="MetaWeblog" blogID="1" preferred="true" apiLink="http://kanng.net/blog/xmlrpc.php" />
+						<api name="Blogger" blogID="1" preferred="false" apiLink="http://kanng.net/blog/xmlrpc.php" />
+					</apis>
+				</service>
+			</rsd>
+		 
