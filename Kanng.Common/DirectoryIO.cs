3c346262dfa142ff50382a6f545aa38bf4ade0fa
jksharp <jiangkang_3@hotmail.com>
2017-03-06 13:54:52 +0800

    sss

---
+++ Kanng.Common/DirectoryIO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Kanng.Common
+{
+    public class DirectoryIO
+    {
+
+        public static void CopyFolder(string from, string to)
+        {
+            if (!Directory.Exists(to))
+                Directory.CreateDirectory(to);
+
+            // 子文件夹
+            foreach (string sub in Directory.GetDirectories(from))
+            CopyFolder(sub + "\\", to + Path.GetFileName(sub) + "\\");
+
+            // 文件
+            foreach (string file in Directory.GetFiles(from))
+            File.Copy(file, to + Path.GetFileName(file), true);
+           
+        }
+    }
+}
